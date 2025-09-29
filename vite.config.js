fd7f61c35e4142754ce9547c0eb37a1d6c1063cf
Mr. Kalwar <techtripengineering-star>
2025-09-29 19:16:30 +0530

    fix: add react plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),       // 👈 this is required for React
+    tailwindcss(), // 👈 this keeps Tailwind working
   ],
 })
